ae9afddae7ec3535782ea8d3b388ca2ffacb1536
Shannarra <petarangelov15@gmail.com>
2023-02-02 16:15:16 +0200

    Added hashes and operations with them

--- src/redis_engine/mod.rs
+++ src/redis_engine/mod.rs
@@ -10,8 +10,10 @@ mod command_execution;
 use command_execution::one_off_command::one_off;
 use command_execution::kvp_command::kvp;
 use command_execution::list_command::list;
+use command_execution::hash_command::hash;
 
 pub const DUMP_FILE_NAME: &str = "dump.my_rdb";
+pub const DEBUG_DUMP_FILE_NAME: &str = "debug_dump.my_rdb";
 
 pub struct ExecutionContext {
     pub key_value_pairs: Arc<Mutex<HashMap<String, RedisValue>>>,
@@ -51,14 +53,15 @@ pub struct Executor {
     pub setup_properly: bool,
 }
 
-pub fn setup_executor() -> Executor {
+pub fn setup_executor(debug_mode: bool) -> Executor {
     use std::{path, fs};
 
     let mut setup_properly = false;
+    let dump_file_path = if debug_mode { &DEBUG_DUMP_FILE_NAME } else { &DUMP_FILE_NAME };
 
-    let path = path::Path::new(&DUMP_FILE_NAME);
+    let path = path::Path::new(&dump_file_path);
     if !path.exists() {
-        if let Err(_) = fs::File::create(&DUMP_FILE_NAME) {
+        if let Err(_) = fs::File::create(&dump_file_path) {
             eprintln!("[ERROR]: Cannot create a dump file!");
         } else {
             let setup_data = r#"{
@@ -72,10 +75,10 @@ pub fn setup_executor() -> Executor {
 
     }
 }"#;
-            fs::write(&DUMP_FILE_NAME, setup_data).unwrap();
+            fs::write(&dump_file_path, setup_data).unwrap();
         }
     } else {
-        if let Ok(text) = fs::read_to_string(&DUMP_FILE_NAME) {
+        if let Ok(text) = fs::read_to_string(&dump_file_path) {
             let context = ExecutionContext::from_file_contents(text);
             setup_properly = true;
 
@@ -141,17 +144,40 @@ impl Executor {
         }
     }
 
-    pub async fn exec(&mut self, command: String) -> command_execution::Result {
+    fn exec_hash_command(&mut self, command: &str, args: Vec<&str>) -> command_execution::Result {
+        match command {
+            "hget"    => { hash::hget(&mut self.context.hashes.lock().unwrap(), args) },
+            "hexists" => { hash::hexists(&mut self.context.hashes.lock().unwrap(), args) },
+            "hdel"    => { hash::hdel(&mut self.context.hashes.lock().unwrap(), args) },
+            "hgetall" => { hash::hgetall(&mut self.context.hashes.lock().unwrap(), args) },
+            "hkeys"   => { hash::hkeys(&mut self.context.hashes.lock().unwrap(), args) },
+            "hlen"    => { hash::hlen(&mut self.context.hashes.lock().unwrap(), args) },
+            "hmset"   => { hash::hmset(&mut self.context.hashes.lock().unwrap(), args) },
+            "hset"    => { hash::hset(&mut self.context.hashes.lock().unwrap(), args) },
+            "hvals"   => { hash::hvals(&mut self.context.hashes.lock().unwrap(), args) },
+            _ => { panic!("This will never be reached"); }
+        }
+    }
 
+    pub async fn exec(&mut self, command: String) -> command_execution::Result {
         let mut command_words = command.split(" ");
         let cmd_name = command_words.nth(0).unwrap();
         let cmd_args = command_words.collect::<Vec<_>>();
 
         match cmd_name {
-            "echo" | "ping" | "flushall" => self.exec_one_off(cmd_name, cmd_args),
-            "set" | "get" | "key" | "type" | "del" | "unlink" | "expire" | "rename"  => self.exec_kvp_command(cmd_name, cmd_args).await,
-            "llen" |"lrem" |"lindex" |"lpop" | "rpop" | "lpush" | "rpush" | "lset" => self.exec_list_command(cmd_name, cmd_args),
-            _ => todo!()
+            "echo" | "ping" | "flushall" => {
+                self.exec_one_off(cmd_name, cmd_args)
+            },
+            "set" | "get" | "key" | "type" | "del" | "unlink" | "expire" | "rename"  => {
+                self.exec_kvp_command(cmd_name, cmd_args).await
+            },
+            "llen" |"lrem" |"lindex" |"lpop" | "rpop" | "lpush" | "rpush" | "lset" => {
+                self.exec_list_command(cmd_name, cmd_args)
+            },
+            "hget"| "hexists"| "hdel"| "hgetall"| "hkeys"| "hlen"| "hmset"| "hset"| "hvals" => {
+                self.exec_hash_command(cmd_name, cmd_args)
+            },
+            _ => Err(format!("Unknown command \"{cmd_name}\" provided."))
         }
     }
 }
